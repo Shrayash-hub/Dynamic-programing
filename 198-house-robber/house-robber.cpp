c4576435e80ed27fbf6af073973d35191380bca0
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-01-21 11:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.35%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& arr) {
+        if(arr.size()==1) return arr[0];
+        if(arr.size()==2) return max(arr[0],arr[1]);
+        
+        vector<int> previous(arr.size());
+        previous[0] = arr[0];
+        previous[1] = max(arr[0],arr[1]);
+        
+        for(int i=2;i<arr.size();i++){
+            previous[i] = max(previous[i-1], arr[i]+previous[i-2]);
+        }
+        return previous[arr.size()-1];
+    }
+};
