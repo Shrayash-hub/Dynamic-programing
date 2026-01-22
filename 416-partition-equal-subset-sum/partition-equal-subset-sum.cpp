f2c4af11743e8b366a0ed7dd89bf9e0eae49e00c
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-01-22 12:12:33 +0530

    Time: 112 ms (65.55%) | Memory: 17.4 MB (59.36%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -8,9 +8,9 @@ public:
         if(sum%2!=0) return false;
         int W = sum/2;
 
-        vector<vector<int>> dp(val.size()+1,vector<int>(W+1,0));
+        vector<vector<bool>> dp(val.size()+1,vector<bool>(W+1,0));
         for(int i=0;i<=val.size();i++){
-            dp[i][0]=1;
+            dp[i][0]=true;
         }
         
         for(int i=1;i<=val.size();i++){
@@ -20,10 +20,10 @@ public:
                 dp[i][j] = dp[i-1][j];
                 // if there are required number of balls -> compare the previous state value and new value
                 else
-                dp[i][j] = (dp[i-1][j]+ dp[i-1][j-val[i-1]])%1000000009 ;
+                dp[i][j] = (dp[i-1][j] || dp[i-1][j-val[i-1]]);
             }
         }
         
-        return dp[val.size()][W]>0;
+        return dp[val.size()][W];
     }
 };
