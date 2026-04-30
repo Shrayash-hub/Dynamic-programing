94b84f5a42b0c5a33656543ee9e87e8bbeced647
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-04-30 06:06:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (46.90%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int perfectSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+
+        // base case
+        for (int i = 0; i < n; i++)
+            dp[i][0] = 1;
+
+        // first element
+        if (arr[0] == 0)
+            dp[0][0] = 2;
+        else if (arr[0] <= target)
+            dp[0][arr[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= target; t++) {
+                int notPick = dp[i - 1][t];
+                int pick = 0;
+
+                if (arr[i] <= t) {
+                    pick = dp[i - 1][t - arr[i]];
+                }
+
+                dp[i][t] = pick + notPick;
+            }
+        }
+        return dp[n - 1][target];
+    }
+
+    int countPartitions(vector<int>& arr, int diff) {
+        // Code here
+        int sum = 0;
+        for (auto it : arr) {
+            sum += it;
+        }
+
+        if (sum - diff < 0 || (sum - diff) % 2 != 0)
+            return 0;
+
+        return perfectSum(arr, (sum - diff) / 2);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // its exactly similar to the question-> Partitions with Given Difference
+        // when we will put positives and negatives assigned elements together -> we will figure out ki x - y k format a ja rhi h chizey , and we need to find number of ways we can get x - y = target
+        return countPartitions(nums,target);
+    }
+};
