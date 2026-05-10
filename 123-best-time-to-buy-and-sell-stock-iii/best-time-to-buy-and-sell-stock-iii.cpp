a43f1f23f0d5e03b51ed1940d5d3d5a9e0ff841f
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-05-10 14:29:21 +0530

    Time: 59 ms (55.67%) | Memory: 79.3 MB (72.82%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1,
-                                       vector<vector<int>>(2, vector<int>(3, 0)));
+        // vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        // optimization
+        vector<vector<int>> after(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
 
         // base cases
         // if cap is zero -> means we have no transaction left so we cannot gain
         // any profit -> index and buy can be anything
-        for (int ind = 0; ind <= n; ind++) {
-            for (int buy = 0; buy <= 1; buy++) {
-                dp[ind][buy][0] = 0;
-            }
-        }
+        // for (int ind = 0; ind <= n; ind++) {
+        //     for (int buy = 0; buy <= 1; buy++) {
+        //         dp[ind][buy][0] = 0;
+        //     }
+        // }
 
         // if index = n then we cannot gain any more profit -> buy and cap can
         // be anything
-        for (int buy = 0; buy <= 1; buy++) {
-            for (int cap = 0; cap <= 2; cap++) {
-                dp[n][buy][cap] = 0;
-            }
-        }
+        // for (int buy = 0; buy <= 1; buy++) {
+        //     for (int cap = 0; cap <= 2; cap++) {
+        //         dp[n][buy][cap] = 0;
+        //     }
+        // }
 
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) {
@@ -33,8 +36,8 @@ public:
                     // price
                     // in both the cases number of transactions will be same as we didnt sold any stockes yet
                     if (buy) {
-                        profit = max(-prices[ind] + dp[ind + 1][0][cap],
-                                     dp[ind + 1][1][cap]);
+                        profit = max(-prices[ind] + after[0][cap],
+                                     after[1][cap]);
                     }
                     // if we cannot buy any stock -> means we have to sell then also
                     // we have 2 options
@@ -43,15 +46,16 @@ public:
                     // 2. or to leave this stock and look for any upcoming price
                     
                      else {
-                        profit = max(prices[ind] + dp[ind + 1][1][cap-1],
-                                     dp[ind + 1][0][cap]);
+                        profit = max(prices[ind] + after[1][cap-1],
+                                     after[0][cap]);
                     }
 
-                    dp[ind][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
             }
+            after = curr;
         }
         //  when index exhausted , two transactions completed 
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
