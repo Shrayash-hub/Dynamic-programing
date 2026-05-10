6433d2ba650d49351b8b6be86302cde3bc1f9ea6
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-05-10 17:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (47.80%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        vector<vector<long long>> dp(n + 2, vector<long long>(2, 0));
+
+        // base case
+        dp[n][0] = 0;
+        dp[n][1] = 0; // if we reached to the last index then there is no profit
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+
+                // if we can buy the stocks then we have to options
+                // 1. either to buy this stock and include the -prices[ind] in
+                // the profit
+                // 2. or to leave this price and lock for any upcoming stock
+                // price
+                if (buy) {
+                    profit = max(-prices[ind] + dp[ind + 1][0], dp[ind + 1][1]);
+                }
+                // if we cannot buy any stock -> means we have to sell then also
+                // we have 2 options
+                // 1. either to sell this stock and include prices[ind] in the
+                // profit
+                // 2. or to leave this stock and look for any upcoming price
+                else {
+                    // we are dealing with ind + 2 , so dp table size should be of [n+2][2]
+                    profit = max(prices[ind] + dp[ind + 2][1], dp[ind + 1][0]);
+                }
+
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+};
