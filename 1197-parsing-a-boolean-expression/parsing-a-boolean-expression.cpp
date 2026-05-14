111b4dd417a7e694ba641d1310003328fed4b068
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-05-14 07:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (99.15%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
     // "!(&(f,t))"
-    // 012 
+    // 012
     // "f"
     // "&(t,f,!(f),t)"
-    bool helper(string& expression, int& idx){
-        //bc
-        if(expression[idx]=='f'){
+    bool helper(string& expression, int& idx) {
+        // bc
+        if (expression[idx] == 'f') {
             idx++;
             return false;
-        }
-        else if(expression[idx]=='!'){
-            idx+=2;
-            //rr
-            bool res= helper(expression,idx);
-            //task
+        } else if (expression[idx] == '!') {
+            idx += 2;
+            // rr
+            bool res = helper(expression, idx);
+            // task
             idx++;
             return !res;
-        }
-        else if(expression[idx]=='&'){
-            bool res=true;
-            idx+=2;
-            while(expression[idx]!=')'){
-                if(expression[idx]==','){
+        } else if (expression[idx] == '&') {
+            bool res = true;
+            idx += 2;
+            while (expression[idx] != ')') {
+                if (expression[idx] == ',') {
                     idx++;
-                }
-                else{
-                   bool temp=helper(expression,idx); 
-                   res= res& temp;
+                } else {
+                    bool temp = helper(expression, idx);
+                    res = res & temp;
                 }
             }
             idx++;
             return res;
         }
         // 0 | 1 | 0=1
-        else if(expression[idx]=='|'){
-            bool res=false;
-            idx+=2;
-            while(expression[idx]!=')'){
-                if(expression[idx]==','){
+        else if (expression[idx] == '|') {
+            bool res = false;
+            idx += 2;
+            while (expression[idx] != ')') {
+                if (expression[idx] == ',') {
                     idx++;
-                }
-                else{
-                   bool temp=helper(expression,idx); 
-                   res= res| temp;
+                } else {
+                    bool temp = helper(expression, idx);
+                    res = res | temp;
                 }
             }
             idx++;
@@ -53,7 +49,7 @@ public:
         return true;
     }
     bool parseBoolExpr(string expression) {
-        int idx=0;
+        int idx = 0;
         return helper(expression, idx);
     }
 };
