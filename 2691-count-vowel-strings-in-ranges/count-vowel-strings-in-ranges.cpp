21198fe77ece2a4f8eaeed2d7e8f8ef12351d412
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-05-21 14:44:26 +0530

    Time: 226 ms (5.09%) | Memory: 74.5 MB (10.70%) - LeetSync

---
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        string vowels = "aeiouAEIOU";
+        return vowels.find(ch) != string::npos;
+    }
+    vector<int> vowelStrings(vector<string>& words,
+                             vector<vector<int>>& queries) {
+        vector<int> prefix(words.size()+1,0);
+        for (int i = 1; i <= words.size(); i++) {
+            if (isVowel(words[i-1][0]) && isVowel(words[i-1][words[i-1].size()-1])) {
+                prefix[i] = prefix[i-1] + 1;
+            }
+            else{
+                prefix[i] = prefix[i-1];
+            }
+        }
+        for(auto it: prefix){
+            cout<<it<<" ";
+        }
+        vector<int> ans;
+        for(auto it: queries){
+            int start = it[0];
+            int end = it[1];
+            ans.push_back((prefix[end+1])-(prefix[start]));
+        }
+        return ans;
+    }
+};
