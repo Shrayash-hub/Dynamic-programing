090a959d5fe2744fe3ea5ce1b6e1fb3943ed089b
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-07-02 20:33:33 +0530

    Time: 7 ms (12.18%) | Memory: 20.8 MB (25.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,72 +1,35 @@
-class Disjoint {
-public:
-    vector<int> parent;
-    vector<int> rank;
-
-public:
-    Disjoint(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    void unionNormal(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX != rootY) {
-            parent[rootY] = rootX;
-        }
-    }
-
-    void unionByRank(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX == rootY)
-            return;
-
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else {
-            parent[rootX] = rootY;
-            rank[rootY]++;
-        }
-    }
-};
-
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        Disjoint ds(isConnected.size());
+    void dfs(vector<int> adj[], vector<bool>& visited, int node) {
+        visited[node] = true;
 
-        for (int i = 0; i < isConnected.size(); i++) {
-            for (int j = 0; j < isConnected[0].size(); j++) {
-                // union the nodes if they are connected 
+        for (auto it : adj[node]) {
+            if (visited[it] == false) {
+                dfs(adj, visited, it);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> adj[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (isConnected[i][j] == 1) {
-                    ds.unionNormal(i, j);
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        int cnt = 0;
-        for (int i = 0; i < isConnected.size(); i++) {
-            // agr koi node khud ka parent h , toh vo ek alag province h
-            if (ds.find(i) == i) {
-                cnt++;
+
+        vector<bool> visited(n, false);
+        int count = 0;
+        for (int v = 0; v < n; v++) {
+            if (visited[v] == false) {
+                count++;
+                dfs(adj, visited, v);
             }
         }
-        return cnt;
+        return count;
     }
 };
