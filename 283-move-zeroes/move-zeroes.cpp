c97cd8f1a06b0ea4e42adad1e218a42a4040181c
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-07-25 20:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (8.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,14 +5,17 @@ public:
         int j = 1;
         int n = nums.size();
         while(i < n && j < n){
+            // if both are zero , then move j by 1 -> so that j reaches to a non zero nums
             if(nums[i] == 0 && nums[j] == 0){
                 j++;
             }
+            // if i at zero and j at non zero then swap -> so that zero come at the end
             else if(nums[i] == 0 && nums[j] != 0){
                 swap(nums[i], nums[j]);
                 i++;
                 j++;
             }
+            // if j at zero and i at not zero , so its already at right position, and if both are non zero then already at right positions
             else{
                 i++;
                 j++;
