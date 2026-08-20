359801071f01e977f606e0dfa82fa1fb1a6f201c
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-08-20 05:51:29 +0530

    Time: 124 ms (89.62%) | Memory: 201.4 MB (95.60%) - LeetSync

--- 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
@@ -1,51 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
+        // lets sort the sources on the basis of colors ( in descending order )
+        // if we will put first ,the sources with larger color values in the queue
+        // then for sure they are going to be processed first 
+        sort(sources.begin(), sources.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[2] > b[2];
+             });
+
         vector<vector<int>> grid(n, vector<int>(m, 0));
-        vector<vector<int>> time(n, vector<int>(m, 0)); // storing when this cell was colored
+        queue<pair<int, pair<int, int>>> q;
 
-        queue<tuple<int,int,int>> q;  // (row, col, color)
-
-        vector<int> dx = {-1, 0, 1, 0};
-        vector<int> dy = {0, 1, 0, -1};
-
-        // initialize sources
-        for (auto& source : sources) {
-            int row = source[0];
-            int col = source[1];
-            int color = source[2];
-
-            grid[row][col] = color;
-            time[row][col] = 1; // as stating points , assign time as 1
-            q.push({row, col, color});
+        // lets push the sources in descending order
+        for (const auto& s : sources) {
+            int r = s[0], c = s[1], color = s[2];
+            if (grid[r][c] == 0) {
+                grid[r][c] = color;
+                q.push({color, {r, c}});
+            }
         }
 
-        // BFS
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
+
+        // starting bfs
         while (!q.empty()) {
-            auto [r, c, col] = q.front();
+            auto [currColor, pos] = q.front();
+            auto [row, clm] = pos;
             q.pop();
 
             for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
+                int newR = row + dx[i];
+                int newC = clm + dy[i];
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    // first time visiting the cell
-                    if (time[nr][nc] == 0) {
-                        // update grid color
-                        grid[nr][nc] = col;
-                        // update time at we reach at this cell
-                        time[nr][nc] = time[r][c] + 1;
-                        q.push({nr, nc, col});
-                    }
-                    // another color reached this cell at same time
-                    else if (time[nr][nc] == time[r][c] + 1) {
-                        // choose the greater color
-                        if (grid[nr][nc] < col) {
-                            grid[nr][nc] = col;
-                            q.push({nr, nc, col});
-                        }
-                    }
+                // now we will see the unvisited cell only , and will not allow the already filled cells ,
+                // as we pushed the sources with larger color values first , they are going to be processed first , and because of this , they are going to fill the unvisited cell first, once a cell is filled larger color value due to its parent cell , there is no need to let fill the color in that cell by the parent with smaller value
+                if (newR >= 0 && newC >= 0 && newR < n && newC < m &&
+                    grid[newR][newC] == 0) {
+                    grid[newR][newC] = currColor;
+                    q.push({currColor, {newR, newC}});
                 }
             }
         }
